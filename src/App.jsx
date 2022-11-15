edbfde439c21b06a31c19ddd4cb1e5d4156bb0e0
bazz <clement.f.bazz@gmail.com>
2022-11-15 12:29:51 +0100

    Update Added dialog

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef } from 'react'
 
 import './App.css'
 import Nav from './App/layOut/Nav'
@@ -6,8 +6,21 @@ import Footer from './App/layOut/Footer'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './App/pages/Home'
 
+// import DOMMethods from './publicTools'
+import Dialog from './App/component/Dialog'
+import LinkBTN from './App/component/LinkBTN'
+import Nft from './App/pages/Nft'
+
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const modal = useRef();
+  function handleModal() {
+    DOMMethods.showModal(modal.current)
+  }
+  function handleClose() {
+    DOMMethods.closeModal(modal.current)
+  }
 
   return (
     <>
@@ -18,10 +31,24 @@ function App() {
         <Routes >
 
           <Route path='/' element={<Home />} />
+          <Route path='NFTs' element={<Nft />} />
 
         </Routes>
-
+        {/* <Dialog modal={modal} >
+          <div className="modal__header">
+            <h3>Connect Wallet</h3>
+            <div className="close" onClick={() => handleClose()}>
+              <Icons name={`cross`} />
+            </div>
+          </div>
+          <div className="modal__body">
+            <p>Choose your preferred wallet:</p>
+            <LinkBTN name={`meta-fox`} text={`Metamask`} />
+            <LinkBTN name={`wallet`} text={`WalletConnect`} />
+          </div>
+        </Dialog> */}
         <Footer />
+
       </Router>
     </>
   )
