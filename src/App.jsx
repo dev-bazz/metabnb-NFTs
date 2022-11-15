515033d0454224fd555be93a3d52b7a8890e496d
bazz <clement.f.bazz@gmail.com>
2022-11-15 11:38:55 +0100

    Removed Styles From App Css

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Nav from './App/layOut/Nav'
+import Footer from './App/layOut/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <>
       <Nav />
       <div>
         <a href="https://vitejs.dev" target="_blank">
+
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
@@ -29,7 +31,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </div>
+      <Footer />
+    </>
   )
 }
 
